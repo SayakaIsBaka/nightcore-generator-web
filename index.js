de8fa91c0df0817fff127d594b76d413d91cc4da
Jérôme Tchan <jerome.tchan@epita.fr>
2020-02-04 22:54:28 +0100

    Progress

--- index.js
+++ index.js
@@ -3,29 +3,37 @@
 const ffmpeg = require("ffmpeg.js/ffmpeg-mp4.js");
 const fs = require("fs");
 
-const speedupSong = () => {
-  let testData = new Uint8Array(fs.readFileSync("test.webm"));
-  var result = ffmpeg({
-    MEMFS: [{
-      name: "in.mp3",
-      data: testData
-    }],
-    arguments: ["-i", "in.mp3", "-filter:a", "\"asetrate=44100*1.25\"", "-vn", "out.mp3"],
-    stdin: function () {},
-  });
+const speedupSong = (file, data) => {
+    var result = ffmpeg({
+        MEMFS: [{
+            name: file,
+            data: data
+        }],
+        arguments: ["-i", file, "-filter:a", "asetrate=44100*1.25", "-vn", "out.mp3"],
+        stdin: function() {},
+    });
 
-  var out = result.MEMFS[0];
-  fs.writeFileSync(out.name, Buffer(out.data));
-}
+    var out = result.MEMFS[0];
+    alert(out);
+};
+
+const fileSelectHandler = evt => {
+    var file = evt.target.files[0];
+    file.arrayBuffer().then(function(data) {
+        speedupSong(file.name, data);
+    });
+};
 
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
+    document.getElementById('audioin').addEventListener('change', fileSelectHandler, false);
 } else {
-  alert('The File APIs are not fully supported in this browser.');
+    alert('The File APIs are not fully supported in this browser.');
 }
 
 /*
 https://github.com/Kagami/ffmpeg.js
 https://github.com/browserify/browserify
 https://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+TODO: compile a custom version of ffmpeg.js
 */
