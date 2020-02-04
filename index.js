43375832324911eaa52a494d86f8151392b3d05e
Jérôme Tchan <jerome.tchan@epita.fr>
2020-02-04 20:02:23 +0100

    Added gitignore

--- index.js
+++ index.js
@@ -3,13 +3,29 @@
 const ffmpeg = require("ffmpeg.js/ffmpeg-mp4.js");
 const fs = require("fs");
 
-let testData = new Uint8Array(fs.readFileSync("test.webm"));
-var result = ffmpeg({
-  MEMFS: [{name: "test.webm", data: testData}],
-  arguments: ["-i", "test.webm", "-filter:a", "\"asetrate=44100*1.25\"", "-vn", "out.mp3"],
-  // Ignore stdin read requests.
-  stdin: function() {},
-});
-// Write out.webm to disk.
-var out = result.MEMFS[0];
-fs.writeFileSync(out.name, Buffer(out.data));
+const speedupSong = () => {
+  let testData = new Uint8Array(fs.readFileSync("test.webm"));
+  var result = ffmpeg({
+    MEMFS: [{
+      name: "in.mp3",
+      data: testData
+    }],
+    arguments: ["-i", "in.mp3", "-filter:a", "\"asetrate=44100*1.25\"", "-vn", "out.mp3"],
+    stdin: function () {},
+  });
+
+  var out = result.MEMFS[0];
+  fs.writeFileSync(out.name, Buffer(out.data));
+}
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
+/*
+https://github.com/Kagami/ffmpeg.js
+https://github.com/browserify/browserify
+https://www.html5rocks.com/en/tutorials/file/dndfiles/
+*/
