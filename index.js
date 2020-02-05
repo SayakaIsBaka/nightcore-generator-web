1dec2a38401dda56c55f5b65523fb0860c69520b
Jérôme Tchan <jerome.tchan@epita.fr>
2020-02-05 22:45:50 +0100

    Fixed file download and encoding format

--- index.js
+++ index.js
@@ -2,23 +2,30 @@
 
 const fs = require("fs");
 
-const createDownloadLink = (file, data) => {
-    const blob = new Blob([data], {type: "audio/mpeg"});
-    const url = URL.createObjectURL(blob);
-    window.location.href = url;
+const saveData = (fileName, data) => {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    const blob = new Blob([data], {type: "audio/ogg"});
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
 };
 
 const speedupSong = (file, data) => {
     var result = ffmpeg_run({
-        arguments: ["-i", file, "-filter:a", "asetrate=44100*1.25", "-vn", "out.mp3"],
+        arguments: ["-i", file, "-filter:a", "asetrate=44100*1.25", "-vn", "out.ogg"],
         files: [{
             name: file,
             data: new Uint8Array(data)
         }],
+        stdin: function(){}
     });
 
     var out = result[0];
-    createDownloadLink(out.name, out.data);
+    saveData(out.name, out.data);
 };
 
 const fileSelectHandler = evt => {
